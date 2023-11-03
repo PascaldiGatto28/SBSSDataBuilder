1281ebffa14d3541c72d47593482851f708ddd66
Richard Levaro <richard.levaro@outlook.com>
2023-11-03 15:38:35 -0700

    Completion of Extensions and DescriptiveStatistics classes. All XML and removal or warnings and suggestions completed.

---
+++ Libraries/Levaro.SBSoftball.Common/DescriptiveStatistics.cs
@@ -0,0 +1,189 @@
+﻿namespace Levaro.SBSoftball.Common
+{
+    /// <summary>
+    /// Encapsulates the descriptive statistics for a sequence of data values. Instances are created via the static method
+    /// <see cref="GetStatistics(IEnumerable{double}, string?, double?)"/>. Generally speaking properties should not be set 
+    /// except via the static method and deserialization.
+    /// </summary>
+    public class DescriptiveStatistics
+    {
+        /// <summary>
+        /// Creates a new instance of the class. Although the static methods are used to programatically create objects, this
+        /// constructor is required for serialization/deserialization.
+        /// </summary>
+        private DescriptiveStatistics()
+        {
+            Title = string.Empty;
+            OrderedSequence = Enumerable.Empty<double>();
+            IsEmpty = true;
+        }
+
+        /// <summary>
+        /// If the value is true, the object should not viewed a successfully instantiated.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets an optional title of the instance of the class.
+        /// </summary>
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the minimum value of the sequence of items.
+        /// </summary>
+        public double Minimum
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the maximum value of the sequence of items.
+        /// </summary>
+        public double Maximum
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the mean (average) of the sequence of items.
+        /// </summary>
+        public double Mean
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the median of the sequence of items.
+        /// </summary>
+        public double Median
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the variance of the sequence of items.
+        /// </summary>
+        public double Variance
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the standard deviation of the sequence of items.
+        /// </summary>
+        public double StdDev
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the number of items in the sequence.
+        /// </summary>
+        public int Count
+        {
+            get;
+            set;
+        }
+
+        /// <summary>
+        /// Gets or sets the sequences of items in ascending order.
+        /// </summary>
+        public IEnumerable<double> OrderedSequence
+        {
+            get;
+            set;
+        }
+
+
+        /// <summary>
+        /// Constructs a <c>DescriptiveStataistics</c> object using the specified sequence of <c>double</c> items.
+        /// </summary>
+        /// <param name="source">The sequence of items of type <c>double</c></param>
+        /// <param name="title"></param>
+        /// <param name="mean"></param>
+        /// <returns></returns>
+        public static DescriptiveStatistics GetStatistics(IEnumerable<double> source, string? title = null, double? mean = null)
+        {
+            DescriptiveStatistics stats = new();
+            if ((source != null) && source.Any())
+            {
+                double sum = 0.0;
+                double sumOfSquares = 0.0;
+                int count = source.Count();
+                double n = (double)count;
+                string description = title ?? $"Statistics for {count:#,###} items";
+
+                double average;
+                double variance;
+                if (mean == null)
+                {
+                    foreach (double value in source)
+                    {
+                        sum += value;
+                        sumOfSquares += (value * value);
+                    }
+
+                    average = sum / n;
+                    variance = (sumOfSquares / n) - (average * average);
+                }
+                else
+                {
+                    foreach (double value in source)
+                    {
+                        double difference = mean.Value - value;
+                        sumOfSquares += (difference * difference);
+                    }
+
+                    average = mean.Value;
+                    variance = sumOfSquares / n;
+                }
+
+                double stdDev = Math.Sqrt(variance);
+                double min = source.ToList().Min();
+                double max = source.ToList().Max();
+                double median = 0.0;
+
+                List<double> orderedList = source.ToList().OrderBy(x => x).ToList();
+                if (count % 2 == 0)
+                {
+                    median = orderedList.Skip((count / 2) - 1).Take(2).Average();
+                }
+                else
+                {
+                    median = orderedList[count / 2];
+                }
+
+                stats = new DescriptiveStatistics()
+                {
+                    IsEmpty = false,
+                    Title = description,
+                    Minimum = Math.Round(min, 3),
+                    Maximum = Math.Round(max, 3),
+                    Mean = Math.Round(average, 3),
+                    Median = Math.Round(median, 3),
+                    Variance = Math.Round(variance, 3),
+                    StdDev = Math.Round(stdDev, 3),
+                    Count = count,
+                    OrderedSequence = orderedList.Select(e => Math.Round(e, 3))
+                    //SourceData = source
+                };
+            }
+
+            return stats;
+        }
+    }
+}
